3eab174c712beb18b054afbea94bc38740427ae3
unknown <godhustler90956@gmail.com>
2023-10-16 17:36:21 +0530

    hero section updated

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -11,28 +11,8 @@ const Intro = () => {
                 <button className={styles.btn}>Explore More</button>
             </div>
 
-            <div className={styles.cardsContainer}>
-                <div className={styles.pin_container}>
-                    <div className={`${styles.card} ${styles.card_small}`}>
-                        <img className={styles.img} src="https://images.unsplash.com/photo-1614036634955-ae5e90f9b9eb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80" alt="img" />
-                    </div>
-                    <div className={`${styles.card} ${styles.card_large}`}>
-                        <img className={styles.img} src="https://images.unsplash.com/photo-1542204165-65bf26472b9b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80" alt="img" />
-                    </div>
-                    <div className={`${styles.card} ${styles.card_medium}`}>
-                        <img className={styles.img} src="https://images.unsplash.com/photo-1618556658017-fd9c732d1360?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1528&q=80" alt="img" />
-                    </div>
-                    <div className={`${styles.card} ${styles.card_medium_medium}`}>
-                        <img className={styles.img} src="https://images.unsplash.com/photo-1579543401509-594bf0e610e4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" alt="img" />
-                    </div>
-                    <div className={`${styles.card} ${styles.card_small}`}>
-                        <img className={styles.img} src="https://images.unsplash.com/photo-1508161773455-3ada8ed2bbec?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80" alt="img" />
-                    </div>
-                    <div className={`${styles.card} ${styles.card_small_small}`}>
-                        <img className={styles.img} src="https://images.unsplash.com/photo-1452639608291-23cd58f6864d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1474&q=80" alt="img" />
-                    </div>
-
-                </div>
+            <div className={styles.imgContainer}>
+                <img className={styles.img} src="https://images.unsplash.com/photo-1491147334573-44cbb4602074?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80" alt="" />
             </div>
         </div>
     )
