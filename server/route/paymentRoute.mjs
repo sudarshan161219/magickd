819c2e0a80a8578446d2bbcf0387a31a6fdaec67
unknown <godhustler90956@gmail.com>
2023-11-06 01:56:27 +0530

    01:56am

--- server/route/paymentRoute.mjs
+++ server/route/paymentRoute.mjs
@@ -4,11 +4,28 @@ const router = Router();
 import {
   checkout,
   paymentVerification,
-  getKey
+  getKey,
 } from "../controllers/paymentController.mjs";
 
 router.route("/checkout").post(checkout);
 router.route("/verification/:Id").post(paymentVerification);
 router.route("/get_key").get(getKey);
+
+router.route("/proxy-razorpay").get((req, res) => {
+  // Define the URL of the Razorpay SDK
+  const razorpayURL = "https://checkout.razorpay.com/v1/checkout.js";
+
+  // Make a request to the Razorpay SDK URL
+  request(razorpayURL, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      // Send the Razorpay SDK content as a response to your client
+      res.header("Content-Type", "application/javascript");
+      res.send(body);
+    } else {
+      // Handle errors as needed
+      res.status(500).send("Error fetching Razorpay SDK");
+    }
+  });
+});
 
 export default router;
